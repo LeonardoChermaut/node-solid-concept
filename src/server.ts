b27baf62b69034085d3d9ecb65abbea2f416c07b
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-19 17:44:01 -0300

    refactor: impl class Server and get env port

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import { app } from "./app";
+import { EnvConfig } from "./helpers/env";
 
-app.listen(3000);
+export class Server {
+    private env: EnvConfig = EnvConfig.getInstance();
+
+    async start() {
+        const port = this.env.get("APP_PORT");
+        app.listen(port, () => console.log(`Server is running on port ${port}.`));
+    }
+}
+
+const server = new Server();
+server.start();
