be53885f5b2c6341320facd0d255b5c9508316ae
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-19 17:34:42 -0300

    add: impl method get property

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,16 +1,19 @@
 import { uuid } from "uuidv4";
 
 export class User {
-    protected readonly id: string;
-    protected name: string;
-    protected email: string;
-    protected password: string;
-    
-    constructor(props: Omit<User, 'id'>, id?: string) {
-        Object.assign(this, props);
-        if (!id) {
-            this.id = uuid();
-        }
+  protected readonly id: string;
+  protected name: string;
+  protected email: string;
+  protected password: string;
+
+  constructor(props: Omit<User, "id">, id?: string) {
+    Object.assign(this, props);
+    if (!id) {
+      this.id = uuid();
     }
-    
+  }
+
+  get getEmail(): string {
+    return this.email;
+  }
 }
