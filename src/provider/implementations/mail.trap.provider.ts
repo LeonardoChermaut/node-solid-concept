e12682ffd09a09cb7fc5da54134df0835c284527
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-19 17:37:10 -0300

    refactor: get direct variable env secret

--- src/provider/implementations/mail.trap.provider.ts
+++ src/provider/implementations/mail.trap.provider.ts
@@ -1,33 +1,41 @@
 import { IMailProvider, IMessage } from "../mail.provider";
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
+import { EnvConfig } from "../../helpers/env";
 
 export class MailTrapMailProvider implements IMailProvider {
     private transporter: Mail;
+    private env: EnvConfig = EnvConfig.getInstance();
 
     constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: "smtp.mailtrap.io",
-            port: 2525,
-            auth: {
-                user: process.env.MAILTRAP_USER,
-                pass: process.env.MAILTRAP_PASS
-            }
-        });
-    }
+        const env = this.env;
+        const host = env.get("MAILTRAP_HOST");
+        const port = env.get("MAILTRAP_PORT");
+        const user = env.get("MAILTRAP_USER");
+        const password = env.get("MAILTRAP_PASS");
 
-    async sendMail(message: IMessage): Promise<void> {
-        await this.transporter.sendMail({
+        this.transporter = nodemailer.createTransport({
+            host,
+            port,
+            auth: {
+                user,
+                password,
+            }
+        } as Mail.Options);
+    }
+    
+    async sendMail({ to, from, subject, body }: IMessage): Promise<void> {
+        return await this.transporter.sendMail({
             to: {
-                name: message.to.name,
-                address: message.to.email
+                name: to.name,
+                address: to.email
             },
             from: {
-                name: message.from.name,
-                address: message.from.email
+                name: from.name,
+                address: from.email
             },
-            subject: message.subject,
-            html: message.body
+            subject: subject,
+            html: body
         });
     }
 }
