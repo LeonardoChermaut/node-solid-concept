143fb64c5793f584e7041dde175c927d524c6349
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-19 21:27:57 -0300

    refactor: user constructor to accept CreateUserRequestDTO as props

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,4 +1,5 @@
 import { uuid } from "uuidv4";
+import { CreateUserRequestDTO } from "../use-case/create-user/create.dto";
 
 export class User {
   protected readonly id: string;
@@ -6,14 +7,14 @@ export class User {
   protected email: string;
   protected password: string;
 
-  constructor(props: Omit<User, "id">, id?: string) {
+  constructor(props: CreateUserRequestDTO, id?: string) {
     Object.assign(this, props);
     if (!id) {
       this.id = uuid();
     }
   }
 
-  get getEmail(): string {
+   getEmail(): string {
     return this.email;
   }
 }
